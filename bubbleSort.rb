bc1ba7d1bec7a52fb4b3c30206e74e784579307b
Bonnet1 <c.mannion84@gmail.com>
2019-06-02 10:14:50 -0400

    Completed second part of bubble sort

--- bubbleSort.rb
+++ bubbleSort.rb
@@ -1,4 +1,4 @@
-def bubbleSort(array)
+def bubble_sort(array)
     swapped = true
     while swapped
         swapped = false
@@ -12,4 +12,22 @@ def bubbleSort(array)
     puts array.inspect
 end
 
-bubbleSort([1, 2, 3, 13, 17, 4, 8, 12])
+bubble_sort([4,3,78,2,0,2])
+
+def bubble_sort_by(array)
+    swapped = true
+    while swapped
+        swapped = false
+        (array.length - 1).times do |i|
+            if yield(array[i], array[i + 1]) > 0
+                array[i], array[i + 1] = array[i + 1], array[i]
+                swapped = true
+            end
+        end
+    end
+    puts array.inspect
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+end
